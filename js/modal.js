76ef2e4882b528d674cf13425da4b818a88fbc4b
syniepolskyi <syniepolskyi@yahoo.com>
2022-05-15 16:28:29 +0300

    First

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+
+        if (document.querySelectorAll("is-hidden").length === 0) {
+            refs.closeModalBtn.focus();
+        }
+
+        return false;
+    }
+})();
